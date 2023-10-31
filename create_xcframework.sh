33f672821c4d0c3a97d629e6b824fa93e78537d9
Daniel Kennett <daniel@cascable.se>
2023-10-31 17:39:36 +0100

    Unify StopKit.framework into one target, enable Mac Catalyst

--- create_xcframework.sh
+++ create_xcframework.sh
@@ -4,11 +4,13 @@ SRCROOT="$(pwd)"
 CONFIGURATION=Release
 BUILT_PRODUCTS_DIR="${SRCROOT}/build"
 
-xcodebuild -scheme "StopKit Mac" -destination "generic/platform=OS X" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}" 1> /dev/null 2>> ./Error.log
+xcodebuild -scheme "StopKit" -destination "generic/platform=macOS,name=Any Mac" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-macosx" 1> /dev/null 2>> ./Error.log
 
-xcodebuild -scheme "StopKit iOS" -destination "generic/platform=iOS Simulator" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-iphonesimulator" 1> /dev/null 2>> ./Error.log
+xcodebuild -scheme "StopKit" -destination "generic/platform=macOS,variant=Mac Catalyst,name=Any Mac" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-maccatalyst" 1> /dev/null 2>> ./Error.log
 
-xcodebuild -scheme "StopKit iOS" -destination "generic/platform=iOS" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-iphoneos" 1> /dev/null 2>> ./Error.log
+xcodebuild -scheme "StopKit" -destination "generic/platform=iOS Simulator" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-iphonesimulator" 1> /dev/null 2>> ./Error.log
+
+xcodebuild -scheme "StopKit" -destination "generic/platform=iOS" -configuration ${CONFIGURATION} BUILD_LIBRARY_FOR_DISTRIBUTION=YES CONFIGURATION_BUILD_DIR="${BUILT_PRODUCTS_DIR}/${CONFIGURATION}-iphoneos" 1> /dev/null 2>> ./Error.log
 
 # Clear previous builds
 DIST_DIR="${SRCROOT}/Distribution"
@@ -28,8 +30,7 @@ function add_framework() {
   fi
 }
 
-add_framework "${SRCROOT}/build/${CONFIGURATION}" "${DIST_DIR}/StopKit-Mac.dSYM"
-for SDK in iphoneos iphonesimulator appletvos appletvsimulator maccatalyst; do
+for SDK in macosx iphoneos iphonesimulator appletvos appletvsimulator maccatalyst; do
   add_framework "${SRCROOT}/build/${CONFIGURATION}-${SDK}" "${DIST_DIR}/StopKit-${SDK}.dSYM"
 done
 
